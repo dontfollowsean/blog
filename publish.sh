014744d3818e76f609849816587828de891e505e
Sean Wilkinson <sean.wilkinson@bouncex.com>
2019-04-16 13:55:01 -0400

    modify publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 
 #!/bin/sh
 
-DIR=$(dirname "$0")
+# DIR=$(dirname "$0")
 
-cd $DIR/..
+# cd $DIR/..
 
 if [[ $(git status -s) ]]
 then
@@ -18,7 +18,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
